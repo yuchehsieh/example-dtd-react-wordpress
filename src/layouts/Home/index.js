0c56ff08136084ab724ae56ccb23ca8fbb7c7947
Murphy Hsieh <g110834005@grad.ntue.edu.tw>
2021-02-24 16:38:28 +0800

    add @wordpress/block-library, axios

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,23 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from '../../components/Header';
 
 import styles from './styles.module.scss';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    let response = await axios.get(
+      'https://dtd.ntue.edu.tw/wp-json/wp/v2/posts?per_page=5',
+    );
+    let data = response.data;
+    setPosts(data);
+  };
+
   return (
     <Fragment>
       <Header />
       <div className={styles.container}>
         <h2>系務公告</h2>
         <div className={styles.postWrapper}>
-          <div className={styles.post}>
-            <p>標題 1</p>
-          </div>
-          <Link className={styles.post} to={`/announcements/12345`}>
-            <p>標題 2</p>
-          </Link>
+          {posts.map((post) => (
+            <Link
+              className={styles.post}
+              to={`/announcements/${post?.id}`}
+              key={post?.id}
+            >
+              <p>{post?.title?.rendered}</p>
+            </Link>
+          ))}
         </div>
       </div>
     </Fragment>
