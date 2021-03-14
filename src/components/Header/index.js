e8e09d62e97652a665c25498cbc1b74c5332aef5
Murphy Hsieh <g110834005@grad.ntue.edu.tw>
2021-03-14 15:47:00 +0800

    feat: install react-bootstrap, BEM in scss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 const Header = () => {
   return (
-    <div className="px-10 py-5 xl:px-64">
-      <h1 className="text-2xl font-bold">數位系 DTD</h1>
+    <div className={styles.container}>
+      <h1>數位系 DTD</h1>
     </div>
   );
 };
+
+// return (
+//   <div className="px-10 py-5 xl:px-64">
+//     <h1 className="text-2xl font-bold">數位系 DTD</h1>
+//   </div>
+// );
 
 // return (
 //   <div className={styles.container}>
--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import Header from '../../components/Header';
 
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -15,7 +15,7 @@ const Home = () => {
 
   const fetchPosts = async () => {
     let url =
-      'https://dtd.ntue.edu.tw/wp-json/wp/v2/posts?categories=31&_fields=id, title, category&per_page=5';
+      'https://dtd.ntue.edu.tw/wp-json/wp/v2/posts?categories=31&_fields=id, title, category, date&per_page=5';
     // let url = 'https://dtd.ntue.edu.tw/wp-json/dtd/v1/announcements';
     let response = await axios.get(url);
     let data = response.data;
@@ -25,18 +25,17 @@ const Home = () => {
   return (
     <Fragment>
       <Header />
-      <div className="px-10 py-10 pt-5 xl:px-64">
-        <h2 className="text-xl">系務公告</h2>
-        <div className="flex-col mt-5 space-y-dtd-spacing">
+      <div className={styles.container}>
+        <h2>系務公告</h2>
+        <div className={styles.posts__wrapper}>
           {posts.map((post) => (
             <Link
-              className="px-dtd-padding py-7 bg-white flex rounded-xl transition duration-150 shadow-lg hover:shadow-xl"
+              className={`${styles.posts_post} flex-column align-items-start `}
               to={`/announcements/${post?.id}`}
               key={post?.id}
             >
-              <p className="text-dtd-general-text-color hover:text-dtd-primary duration-150">
-                {post?.title?.rendered}
-              </p>
+              <p>{post?.title?.rendered}</p>
+              <sub>{DateFormatter(post?.date)}</sub>
             </Link>
           ))}
         </div>
@@ -44,6 +43,40 @@ const Home = () => {
     </Fragment>
   );
 };
+
+const DateFormatter = (date) => {
+  const dateObj = new Date(date);
+
+  let year = dateObj.getFullYear();
+  let month = dateObj.getMonth();
+  let day = dateObj.getDay();
+
+  let dateStr = `${year}-${month}-${day}`;
+
+  return dateStr;
+};
+
+// return (
+//   <Fragment>
+//     <Header />
+//     <div className="px-10 py-10 pt-5 xl:px-64">
+//       <h2 className="text-xl">系務公告</h2>
+//       <div className="flex-col mt-5 space-y-dtd-spacing">
+//         {posts.map((post) => (
+//           <Link
+//             className="px-dtd-padding py-7 bg-white flex rounded-xl transition duration-150 shadow-lg hover:shadow-xl"
+//             to={`/announcements/${post?.id}`}
+//             key={post?.id}
+//           >
+//             <p className="text-dtd-general-text-color hover:text-dtd-primary duration-150">
+//               {post?.title?.rendered}
+//             </p>
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   </Fragment>
+// );
 
 // return (
 //   <Fragment>
